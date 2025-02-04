42a8737286c1aebbdad698088a6fda7112d6f8f7
shivamjadham <163400942+shivamjadham@users.noreply.github.com>
2025-02-04 20:35:38 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -7,7 +7,8 @@ const listingSchema = Joi.object({
         location:Joi.string().required(),
         country:Joi.string().required(),
         price:Joi.number().required().min(0),
-        image:Joi.string().allow("",null)
+        image:Joi.string().allow("",null),
+        category:Joi.string().allow("",null)
     }).required()
 });
 
